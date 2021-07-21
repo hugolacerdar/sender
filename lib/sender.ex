74e2aeae2783f77c9e0c6eca42c894906ef0f64f
Hugo <hugolacerdar@gmail.com>
2021-07-21 09:45:27 -0300

    Add email delivery base logic

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,2 +1,17 @@
 defmodule Sender do
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    emails
+    |> Enum.map(fn email ->
+      Task.async(fn ->
+        send_email(email)
+      end)
+    end)
+    |> Enum.map(&Task.await/1)
+  end
 end
