ce1f681e0df883409ce00bf71abd5540ae1b9374
Hugo <hugolacerdar@gmail.com>
2021-07-21 11:15:09 -0300

    Add fault tolerance w async stream nolink

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,4 +1,8 @@
 defmodule Sender do
+  def send_email("jkl@email.com" = email) do
+    raise "Oops, couldn't send email to #{email}!"
+  end
+
   def send_email(email) do
     Process.sleep(3000)
     IO.puts("Email to #{email} sent")
@@ -6,8 +10,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1, ordered: false)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1, ordered: false)
     |> Enum.to_list()
   end
 end
