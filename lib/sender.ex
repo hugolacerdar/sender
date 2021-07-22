bc802b8e4b9c0b01e9e6c22b7b113c133e08c8a5
Hugo <hugolacerdar@gmail.com>
2021-07-22 14:20:03 -0300

    Update send email error case

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,7 +1,5 @@
 defmodule Sender do
-  def send_email("jkl@email.com" = email) do
-    raise "Oops, couldn't send email to #{email}!"
-  end
+  def send_email("jkl@email.com" = _email), do: :error
 
   def send_email(email) do
     Process.sleep(3000)
